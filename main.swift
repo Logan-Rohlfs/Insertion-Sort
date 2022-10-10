4d61cba6a47df7f85ea9634e7ac7ba440d450156
Logan-Rohlfs <loganrohlfs76@gmail.com>
2022-10-10 08:13:10 -0500

    Final

--- main.swift
+++ main.swift
@@ -1,37 +1,41 @@
+#!/opt/swift-current/swift
 import Swift
-// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
-// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
-let unsortedIntegers = [5, 1, 4, 2, 8]
-// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
-// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+var unsortedStrings = [String]()
 
-// Add your code below:
+while let word = readLine() {
+    if word == "" {
+        break
+    } else {
+        unsortedStrings.append(word.lowercased())
+    }
+}
 
-func insertionSort(_ array: [Int]) {
-    var ints = array
+
+func insertionSort(_ array: [String]) {
+    var strings = array
     var pass_total = 0
     var swaps_current = 0
     var swaps_total = 0
     
-    print("Pass: \(pass_total), Swaps: \(swaps_current)/\(swaps_total), Array: \(ints)")
+    //print("Pass: \(pass_total), Swaps: \(swaps_current)/\(swaps_total), Array: \(ints)")
     
-    for x in 1..<ints.count {
+    for x in 1..<strings.count {
         var y = x
-        let temp = ints[y]
-        while y > 0 && temp < ints[y - 1] {
-            ints[y] = ints[y - 1]
+        let temp = strings[y]
+        while y > 0 && temp < strings[y - 1] {
+            strings[y] = strings[y - 1]
             y -= 1
             swaps_current += 1
             swaps_total += 1
         }
-        ints[y] = temp
+        strings[y] = temp
         pass_total += 1
 
-        print("Pass: \(pass_total), Swaps: \(swaps_current)/\(swaps_total), Array: \(ints)")
+        //print("Pass: \(pass_total), Swaps: \(swaps_current)/\(swaps_total), Array: \(ints)")
         
         swaps_current = 0
 
     }
 }
 
-insertionSort(unsortedIntegers)
+insertionSort(unsortedStrings)
